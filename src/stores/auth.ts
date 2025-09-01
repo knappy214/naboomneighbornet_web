4c13ffae4e358b36d4cd4b2e26464cfd831cb0cd
Peter Knapton <peter.knapton@gmail.com>
2025-09-01 11:08:12 +0200

    Try to Merge changes

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,8 +2,27 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => ({ accessToken: '', refreshToken: '' }),
   actions: {
+<<<<<<< HEAD
     setAccessToken(t: string) {
       this.accessToken = t
+=======
+    async login(credentials: Credentials) {
+      try {
+        const response = await axios.post('/api/v2/auth/login/', credentials)
+        this.token = response.data.token
+        this.user = response.data.user
+        this.isAuthenticated = true
+
+        if (this.token) {
+          localStorage.setItem('token', this.token)
+          axios.defaults.headers.common['Authorization'] = `Token ${this.token}`
+        }
+
+        return response.data
+      } catch (error) {
+        throw error
+      }
+>>>>>>> 538ed48 (Try to Merge changes)
     },
     setRefreshToken(t: string) {
       this.refreshToken = t
